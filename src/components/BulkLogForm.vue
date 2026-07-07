7e718c416d6d8b1bb5ba966618dbeb565a30b48e
Nhan Le <nhan.le@commissionfactory.com>
2026-07-07 23:23:11 +0700

    refactor(home): use useTimeLogs + ISO dates, drop month sharding

--- src/components/BulkLogForm.vue
+++ src/components/BulkLogForm.vue
@@ -15,7 +15,7 @@ import { array, date, number, object, string } from 'yup';
 
 import dayjs from 'dayjs';
 
-import { shortDateFormat } from '@/common/DateFormat';
+import { isoDateFormat } from '@/common/DateFormat';
 import { minutesToHourWithMinutes } from '@/common/DateHelpers';
 import { useSettingsStore } from '@/stores/settings';
 import { nanoid } from 'nanoid';
@@ -150,7 +150,7 @@ const onSave = handleSubmit((values) => {
   if (isEditMode.value && editingLog) {
     const updatedLog: TimeLog = {
       id: editingLog.id, // Keep existing ID
-      date: dayjs(values.selectedDates[0]).format(shortDateFormat),
+      date: dayjs(values.selectedDates[0]).format(isoDateFormat),
       project: values.project!,
       task: values.task!,
       duration,
@@ -163,7 +163,7 @@ const onSave = handleSubmit((values) => {
     // Create mode: Create multiple logs for selected dates
     const logs = values.selectedDates.map((date) => ({
       id: nanoid(),
-      date: dayjs(date).format(shortDateFormat),
+      date: dayjs(date).format(isoDateFormat),
       project: values.project!,
       task: values.task!,
       duration,
@@ -260,7 +260,7 @@ watch(
       const catName = sortedCategories.value.find((c) => c.id === existingProject?.categoryId)?.name;
       resetForm({
         values: {
-          selectedDates: dates.length > 0 ? dates : [dayjs(editing.date, shortDateFormat).toDate()],
+          selectedDates: dates.length > 0 ? dates : [dayjs(editing.date, [isoDateFormat, 'MM/DD/YYYY']).toDate()],
           project: editing.project,
           task: editing.task,
           duration: editing.duration,
